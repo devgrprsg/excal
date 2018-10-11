e784f9b23902c366956cdfe556e298472bf8ca16
unknown <ritusinglaa08@gmail.com>
2018-10-11 16:34:24 +0530

    i am finalising 3 apis

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
+
 const functions = require('firebase-functions');
 const admin=require('firebase-admin');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-
+const crypto = require('crypto');
 admin.initializeApp();
 const database=admin.database();
 const app = express();
@@ -12,38 +13,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 //hard-coded string
 let notification="posted on timeline";
 
-const authenticate = (req, res, next) => {
-     if(req.body.accessToken === undefined || req.body.accessToken === '')
-    {
-        console.log("No accessToken");
-        return res.json({error : true})
-    }
-     let accessToken = req.body.accessToken;
-     console.log(accessToken);
-     jwt.verify(accessToken,"RSG",(err,data) => {
-         if(err)
-        {
-            console.log("i am here");
-            return res.json({error : true})
-        }
-        else
-        {
-            if(data.error !== null)
-            {
-                return res.json({
-                    authenticatedRequest : false
-                })
-            }
-            else
-            {
-                let email1 = body.emails[0].value.split(/[@]/)[0];
-                let email = email1.replace(/\./g,',');
-                req.body.email = email;
-                return next();
-            }
-        }
-    })
-}
+
 
 exports.getFriends = functions.https.onRequest((request , response) => {
     var email=request.query.email;
@@ -71,61 +41,76 @@ exports.getFriends = functions.https.onRequest((request , response) => {
      response.status(200).json(items);
 });
 
-exports.addtimeline=functions.https.onRequest((req , res) => {
-    console.log(req.query);
-    let email=req.query.email;
-    let link=req.query.link;
-    let description=req.query.description;
-    let node="posts";
-    let ref=database.ref();
+exports.addtimeline = functions.https.onRequest((req , res) => {
+    let email = req.body.email;
+    let link = req.body.link;
+    let description = req.body.description;
+    let node = "posts";
+    let ref = database.ref();
     //post added to posts node.
-    let postId=ref.child(node).push().key;
+    let hashkey = link+email;
+    let postId = crypto.createHash('md5').update(hashkey).digest('hex');
     console.log(postId);
     admin.database().ref().child(node+"/"+postId).set({
         uname: email,
-        like: 0,
-        comment: ' ',
         description: description,
         link: link
     });
     //adding postId to uname timeline.
-    let node2="users/"+email;
+    let node2 = "users/"+email;
     ref.child(node2+"/timeline").push(postId);
     ref.child(node2+"/friends").once('value',(snapshot) => {
-        let node3="users";
+        let node3 = "users";
         snapshot.forEach(function(childSnapshot) {
             ref.child(node3+"/"+childSnapshot.val()+"/timeline").push(postId);
             ref.child(node3 +"/"+childSnapshot.val() + "/notifications").push(email+" "+notification);
         });
-    })
-    res.status(200).json({
-        message: "post added!"
-    })
-});
-
-exports.sendFriendRequest=functions.https.onRequest((req,res) => {
-    let receiverEmail=req.query.receiverEmail;
-    let senderEmail=req.query.email;
-    let node="users/"+receiverEmail+"/FriendRequests/"+senderEmail;
-    let ref=database.ref();
-    ref.child(node).set({ bool : 1});
-    res.status(200).json({
-        message: "friend request sent!"
+    }).then((snap) => {
+            return  res.json({
+                success: true,
+                message: "post added!",
+                postId: postId         
+        })
+    }).catch((err) => {
+            return res.json({
+                success: false,
+                message: err
+        })
     })
 });
 
-exports.acceptFriendRequest=functions.https.onRequest((req,res) => {
-    let senderEmail=req.query.senderEmail;
-    let receiverEmail=req.query.email;
-    let node1="users/";
-    let node2=node1+"/"+receiverEmail+"/friends";
-    let ref=database.ref();
+exports.sendFriendRequest = functions.https.onRequest((req,res) => {
+    let receiverEmail = req.query.receiverEmail;
+    let senderEmail = req.query.email;
+    let node = "users/" + receiverEmail + "/FriendRequests/" + senderEmail;
+    let ref = database.ref();
+    ref.child(node).set({ bool : 1}).then((snap) => {
+        return res.status(200).json({
+            success: true,
+            message: "friend request sent!"
+        })
+    }).catch((err) => {
+        return res.json({
+            success: false,
+            message: err
+        })
+    })
+});
+
+exports.acceptFriendRequest = functions.https.onRequest((req,res) => {
+    let senderEmail = req.query.senderEmail;
+    let receiverEmail = req.query.email;
+    let node1 = "users/";
+    let node2 = node1+"/"+receiverEmail+"/friends";
+    let ref = database.ref();
     ref.child(node2).push(senderEmail);
-    let node3=node1+"/"+senderEmail+"/friends";
+    let node3 = node1+"/"+senderEmail+"/friends";
     ref.child(node3).push(receiverEmail);
-    let node4=node1+"/"+receiverEmail+"/FriendRequests/"+senderEmail;
-    ref.child(node4).remove();
-    res.status(200).json({
-        message: "friend request accepted"
+    let node4 = node1+"/"+receiverEmail+"/FriendRequests/"+senderEmail;
+    ref.child(node4).remove().catch((err) => {
+         return res.json({
+            success: false,
+            message: err
+        })
     })
 });
